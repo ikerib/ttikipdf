c01b754a00732a7d7844f834e3beaef075fb68df
Iker Ibarguren <ikerib@gmail.com>
2016-11-19 19:07:34 +0100

    Liburutegiak instalatzen

---
+++ spec/AppBundle/Menu/BuilderSpec.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ikerib
+ * Date: 19/11/2016
+ * Time: 18:35
+ */
+
+namespace spec\AppBundle\Menu;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class BuilderSpec extends ObjectBehavior
+{
+}
---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class Builder implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+
+        $menu->addChild('Home', array('route' => 'homepage'));
+
+        // access services from the container!
+        $em = $this->container->get('doctrine')->getManager();
+        // findMostRecent and Blog are just imaginary examples
+
+
+        // ... add more children
+
+        return $menu;
+    }
+}
